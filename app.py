913c187528970521427fb5e2fb7b5b298fc06f73
TKM-Ma <takami3717@gmail.com>
2026-07-01 23:19:15 +0900

    expander and data_editor

--- app.py
+++ app.py
@@ -22,12 +22,32 @@ budget, threshold = show_sidebar()
 for group in groups:
     st.subheader(group.name)
 
-    for item in group.items:
-        item.score = st.selectbox(
-            item.name,
-            range(1,11),
-            key=F"{group.name}_{item.name}"
+    with st.expander(group.name):
+        df = pd.DataFrame({
+            "キャラ名":[
+                item.name for item in group.items
+            ],
+            "評価":[
+                item.score for item in group.items
+            ]
+        })
+        
+        edited = st.data_editor(
+            df,
+            disabled=["キャラ名"],
+            column_config={
+                "評価": st.column_config.SelectboxColumn(
+                    "評価",
+                    options=list(range(1, 11)),
+                    required=True
+                )
+            },
+            hide_index=True
         )
+        
+    for item, (_, row) in zip(group.items, edited.iterrows()):
+        item.score = row["評価"]
+
 if st.button("おすすめを計算"):
         show_recommendation(groups, budget, threshold)
 
