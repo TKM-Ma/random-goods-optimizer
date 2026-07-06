c7194abc7fa218dead4ab4505da4895567fadd70
TKM-Ma <takami3717@gmail.com>
2026-07-06 18:01:10 +0900

    add description

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 
 from dataio.loader import load_all_groups, load_template, apply_score
 from dataio.saver import groups_to_json
@@ -10,7 +11,8 @@ from ui import (
     show_sidebar,
     show_input,
     show_statistics,
-    show_recommendation
+    show_recommendation,
+    show_description,
 )
 
 title = "ウマ娘　ランダムグッズ購入数計算ツール"
@@ -21,49 +23,66 @@ st.set_page_config(
 
 st.title(title)
 
-if "groups" not in st.session_state:
-    st.session_state.groups = load_template("templates/umamusume_realCapsuleToy/real_capsule_toy.json")
-groups = st.session_state.groups
+show_description()
 
-budget, threshold = show_sidebar()
+templates = [
+    d for d in os.listdir("templates")
+    if os.path.isdir(os.path.join("templates", d))
+]
 
-uploaded = st.file_uploader(
-    "評価ファイル",
-    type="json",
-)
-if st.button("ファイルを読み込む"):
-    if uploaded is not None:
-        apply_score(uploaded)
-
-show_input()
-
-json_data = groups_to_json(groups)
-
-st.download_button(
-    "スコアを保存",
-    data=json_data,
-    file_name="score.json",
-    mime="application/json"
+selected = st.selectbox(
+    "作品を選択",
+    ["選択してください"] + templates
 )
 
-if st.button("おすすめを計算"):
-    if Statistics.find_unrated_items(groups):
-        st.error("未評価のキャラがあります")
-        st.stop()
-    show_recommendation(groups, budget, threshold)
+if selected != "選択してください":
+    if (
+        "selected_template" not in st.session_state
+        or st.session_state.selected_template != selected
+    ):
+        path = f"templates/{selected}/template.json"
+        st.session_state.groups = load_template(path)
+    groups = st.session_state.groups
 
-def excel():
-    upload_file = st.file_uploader(
-        "Exelファイルを選択してください",
-        type=["xlsx"]
+    budget, threshold = show_sidebar()
+
+    uploaded = st.file_uploader(
+        "評価ファイル",
+        type="json",
+    )
+    if st.button("ファイルを読み込む"):
+        if uploaded is not None:
+            apply_score(uploaded)
+
+    show_input()
+
+    json_data = groups_to_json(groups)
+
+    st.download_button(
+        "スコアを保存",
+        data=json_data,
+        file_name="score.json",
+        mime="application/json"
     )
 
-    if upload_file is not None:
-        groups = load_all_groups(upload_file)
+    if st.button("おすすめを計算"):
+        if Statistics.find_unrated_items(groups):
+            st.error("未評価のキャラがあります")
+            st.stop()
+        show_recommendation(groups, budget, threshold)
 
-        budget, threshold = show_sidebar()
-        
-        show_statistics(groups, threshold)
+    def excel():
+        upload_file = st.file_uploader(
+            "Exelファイルを選択してください",
+            type=["xlsx"]
+        )
 
-        if st.button("おすすめを計算"):
-            show_recommendation(groups, budget, threshold)
+        if upload_file is not None:
+            groups = load_all_groups(upload_file)
+
+            budget, threshold = show_sidebar()
+            
+            show_statistics(groups, threshold)
+
+            if st.button("おすすめを計算"):
+                show_recommendation(groups, budget, threshold)
